2b4ceb2756ad24c636e6875e5d397c7620e8b06c
lw3b3r <daevahush@gmail.com>
2021-04-28 15:05:49 -0400

    Initial Commit

---
+++ war/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  war
+//
+//  Created by Lin on 2/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    //Generate a random number between 2 & 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    //Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    //Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                
+                HStack() {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                        .padding(.bottom, 10)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
+
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war/warApp.swift
@@ -0,0 +1,17 @@
+//
+//  warApp.swift
+//  war
+//
+//  Created by Lin on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct warApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
